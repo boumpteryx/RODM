691e7020682d12e34009d3c9610734d67c0c437e
Martin Lainée <martin.lainee@eleves.enpc.fr>
2022-04-11 19:09:05 +0200

    Ajouté nouvelles fonctions distance

--- src/struct/distance.jl
+++ src/struct/distance.jl
@@ -1,4 +1,5 @@
 using Distances
+using Random
 
 """
 Représente une distance entre deux données identifiées par leur id
@@ -30,3 +31,53 @@ function Distance(id1::Int, id2::Int, x::Matrix{Float64})
     return d
     
 end
+
+
+##### Norme 1
+
+# function Distance(id1::Int, id2::Int, x::Matrix{Float64})
+
+#     d = Distance()
+#     d.distance = sum(abs(x[id1,i] -x[id2,i]) for i in 1:size(x)[2])
+#     d.ids = [id1,id2]
+
+#     return d
+
+# end
+
+##### Norme infinie
+
+# function Distance(id1::Int, id2::Int, x::Matrix{Float64})
+
+#     d = Distance()
+#     d.distance = max(abs(x[id1,i] -x[id2,i]) for i in 1:size(x)[2])
+#     d.ids = [id1,id2]
+
+#     return d
+
+# end
+
+##### Pseudo-norme min
+
+# function Distance(id1::Int, id2::Int, x::Matrix{Float64})
+
+#     d = Distance()
+#     d.distance = min(abs(x[id1,i] -x[id2,i]) for i in 1:size(x)[2])
+#     d.ids = [id1,id2]
+
+#     return d
+
+# end
+
+##### Norme 2 perturbée
+
+# function Distance(id1::Int, id2::Int, x::Matrix{Float64})
+
+#     d = Distance()
+#     acc = euclidean(x[id1, :], x[id2, :])
+#     d.distance = acc * (0.6 + 0.4*rand())
+#     d.ids = [id1,id2]
+
+#     return d
+
+# end
